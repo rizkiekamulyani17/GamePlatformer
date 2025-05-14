d61d27115687240abf5362cd67d7619ccb2b5234
Rizki Eka Mulyani <rizkiekamulyani123@gmail.com>
2025-05-14 11:05:18 +0700

    commite game

---
+++ gamedevpraktikum/Assets/scrip/tes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tes : MonoBehaviour
+{
+    // public GameObject player;
+    // private void Awake(){
+    //     player.SetActive(true);
+    // } fungsi untuk tanpa menenkan play langsung jalan sendiri
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
